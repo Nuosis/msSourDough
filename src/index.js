63f29388834c872ff817098db3da033a9d6024ca
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-05-18 18:44:49 -0700

    working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-//Here we're importing items we'll need. You can add other imports here.
+//https://observablehq.com/@mourner/sourdough-calculator
+window.hours_to_ferment= function(proof_temp, levain) {
+  const f = proof_temp * 9 / 5 + 32;
+  const result = Math.log(levain / 100 / 0.894) * (-0.0000336713 * f ** 4 + 0.0105207916 * f ** 3 - 1.2495985607 * f ** 2 + 67.0024722564 * f - 1374.6540546564);
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+  FileMaker.PerformScript("presentResult", JSON.stringify(result));
+}
